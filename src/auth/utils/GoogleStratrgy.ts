b49e9ea15575401b0873507c4bde1243bbe8cbba
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2023-03-03 14:08:42 +0100

    [chore]:Google validation handler

--- src/auth/utils/GoogleStratrgy.ts
+++ src/auth/utils/GoogleStratrgy.ts
@@ -1,6 +1,8 @@
+import { Injectable } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
-import { Strategy } from "passport-google-oauth20";
+import { Profile, Strategy } from "passport-google-oauth20";
 
+@Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy){
     constructor(){
         super({
@@ -9,5 +11,9 @@ export class GoogleStrategy extends PassportStrategy(Strategy){
             callbackURL: 'http://localhost:3001/api/auth/google/redirect',
             scope: ['profile', 'email'],
         });
+    }
+
+    async validate(accessToken: string, refreshToken: string, profile: Profile){
+        
     }
 }
