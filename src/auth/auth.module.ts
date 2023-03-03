89dc784d07c9d72b0049e6e813f1ccbaba5105f3
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2023-03-03 05:33:22 +0100

    [chore]:module integration

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
 
-@Module({})
+@Module({
+    controllers: [AuthController],
+    providers: []
+})
 export class AuthModule {}
