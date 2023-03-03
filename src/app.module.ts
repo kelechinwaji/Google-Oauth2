189fbb303aa6bdabc4725212674d0adfc01bb1d2
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2023-03-03 18:32:45 +0100

    [feat]:database configurations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'auto_user',
+    password: 'admin04',
+    database: 'grit',
+    entities: [],
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,16 +1,19 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, UseGuards } from "@nestjs/common";
+import { GoogleAuthGuard } from "./utils/Guards";
 
 
 @Controller('auth')
 export class AuthController {
 
     @Get('google/login')
+    @UseGuards(GoogleAuthGuard)
     handleLogin(){
        return {msg: 'Google Authentication'}
     }
 
     // api/auth/google/redirect
     @Get('google/redirect')
+    @UseGuards(GoogleAuthGuard)
     handleRedirect(){
         return {msg: 'OK'}
     }
