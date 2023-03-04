358f42cca1e90c6908ac2e61e4b3f22028f73550
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2023-03-04 16:59:46 +0100

    [chore]:user validation service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,1 +1,13 @@
+import { InjectRepository } from "@nestjs/typeorm";
+import { User } from "src/TypeOrm/entities/user";
+import { UserDetails } from "src/utils/types";
+import { Repository } from "typeorm";
 
+export class AuthService {
+    constructor(@InjectRepository(User) private readonly userRepository: Repository<User>){}
+
+    validateUser(details: UserDetails){
+      
+        
+    }
+}
