26ee0a91dace49d8870effab745b3e123930adea
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2023-03-04 17:22:13 +0100

    [chore]:google user details saved to the DB

--- src/auth/utils/GoogleStratrgy.ts
+++ src/auth/utils/GoogleStratrgy.ts
@@ -1,10 +1,13 @@
-import { Injectable } from "@nestjs/common";
+import { Inject, Injectable } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
 import { Profile, Strategy } from "passport-google-oauth20";
+import { AuthService } from "../auth.service";
 
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy){
-    constructor(){
+    constructor(
+        @Inject('AUTH_SERVICE') private readonly authService: AuthService,
+    ){
         super({
             clientID: '779234031482-g1rmbd3emgsh8eh1nj13969v51c7ftcr.apps.googleusercontent.com',
             clientSecret: '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',
@@ -17,6 +20,10 @@ export class GoogleStrategy extends PassportStrategy(Strategy){
         console.log(accessToken, 'access');
         console.log(refreshToken, 'Refresh');
         console.log(profile, 'profillle');
+        const user = await this.authService.validateUser({
+            email: profile.emails[0].value,
+            displayName: profile.displayName
+        })
          
     }
 }
