6be11702272625ccad6c19652c16b57ba69c6942
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2023-03-10 22:58:11 +0100

    [chore]:session secrets

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
   app.use(
     session({
-      secret: "amksdclkmewiewfwedmklwek",
+      secret: process.env.SECRET,
       saveUninitialized: false,
       resave: false,
       cookie: {
